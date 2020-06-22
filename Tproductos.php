1e5af41644f5f7ba07bae613111c0399df06a66c
Eduardo Ojeda Hernandez <eduardo.923@hotmail.com>
2020-06-22 16:13:16 -0500

    Fix: Se agrego Editar, tiene el mismo error que el otro editarProvedor

--- Tproductos.php
+++ Tproductos.php
@@ -218,7 +218,7 @@
 
 
                                                     <td>
-                                                    <a href="TeditarP.php?id=<?php echo $row["id_prod"]; ?>" type="button" aria-haspopup="true" aria-expanded="false" class="btn-shadow btn btn-info">Editar
+                                                    <a href="TeditarPro.php?id=<?php echo $row["id_prod"]; ?>" type="button" aria-haspopup="true" aria-expanded="false" class="btn-shadow btn btn-info">Editar
                                                                 </a>
                                                     <a href="funciones/elimiar_pro.php?id=<?php echo $row["id_prod"]; ?>" type="button" aria-haspopup="true" aria-expanded="false" class="btn-shadow btn btn-danger" onclick="return confirmar()">Eliminar
                                                                 </a>
