7052f6ea22f79b7eb3b2adf31e79805201cf2ab9
CarlosJesusMaldonadoPAtino <carlosmaldonado1_7@hotmail.com>
2020-06-27 18:25:52 -0500

    tabla-categoria

--- tabla-categoria.php
+++ tabla-categoria.php
@@ -1,6 +1,18 @@
 <?php
-include("funciones/db.php");
-$cat = "SELECT * FROM categoria";
+require_once '../../config/config.php';
+require_once ROOT_PATH . '/libs/database.php';
+session_start();
+error_reporting(0);
+$id_usr = $_SESSION['id'];
+if (isset($id_usr)) {
+    //Traer id del modulo actual
+    $idModuloCostos = $db->select("modulos", "id_modulo", ["nombre_modulo" => "costos"]);
+    //Si no puede consultar este modulo mostrar pagina de error
+    if (!in_array($idModuloCostos[0], $_SESSION["consultar"])) {
+        header("Location:" . URL . "/403.html");
+    } else {
+// include("./funciones/db.php");
+// $cat = "SELECT * FROM categoria";
 ?>
 
 <!doctype html>
@@ -15,6 +27,9 @@ $cat = "SELECT * FROM categoria";
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
     <meta name="description" content="Tables are the backbone of almost all web applications.">
     <meta name="msapplication-tap-highlight" content="no">
+    <link rel="stylesheet" href="<?php echo constant('URL') ?>/main.css" />
+        <link rel="stylesheet" href="<?php echo constant('URL') ?>/style.css" />
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
     <link href="./main.css" rel="stylesheet">
 </head>
     <script type="text/javascript">
@@ -32,120 +47,10 @@ $cat = "SELECT * FROM categoria";
         
     </script>
 <body>
-    <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
-        <div class="app-header header-shadow">
-            <div class="app-header__logo">
-                <div class="logo-src"></div>
-                <div class="header__pane ml-auto">
-                    <div>
-                        <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
-                            <span class="hamburger-box">
-                                <span class="hamburger-inner"></span>
-                            </span>
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div class="app-header__mobile-menu">
-                <div>
-                    <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
-                        <span class="hamburger-box">
-                            <span class="hamburger-inner"></span>
-                        </span>
-                    </button>
-                </div>
-            </div>
-            <div class="app-header__menu">
-                <span>
-                    <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
-                        <span class="btn-icon-wrapper">
-                            <i class="fa fa-ellipsis-v fa-w-6"></i>
-                        </span>
-                    </button>
-                </span>
-            </div>
-            <div class="app-header__content">
-                <div class="app-header-left">
-                    <div class="search-wrapper">
-                        <div class="input-holder">
-                            <input type="text" class="search-input" placeholder="Type to search">
-                            <button class="search-icon"><span></span></button>
-                        </div>
-                        <button class="close"></button>
-                    </div>
-                </div>
-            </div>
-        </div>
+<div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
+    <?php include(ROOT_PATH . "/includes/navbar.php"); ?>
         <div class="app-main">
-            <div class="app-sidebar sidebar-shadow">
-                <div class="app-header__logo">
-                    <div class="logo-src"></div>
-                    <div class="header__pane ml-auto">
-                        <div>
-                            <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
-                                <span class="hamburger-box">
-                                    <span class="hamburger-inner"></span>
-                                </span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div class="app-header__mobile-menu">
-                    <div>
-                        <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
-                            <span class="hamburger-box">
-                                <span class="hamburger-inner"></span>
-                            </span>
-                        </button>
-                    </div>
-                </div>
-                <div class="app-header__menu">
-                    <span>
-                        <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
-                            <span class="btn-icon-wrapper">
-                                <i class="fa fa-ellipsis-v fa-w-6"></i>
-                            </span>
-                        </button>
-                    </span>
-                </div>
-                <div class="scrollbar-sidebar">
-                    <div class="app-sidebar__inner">
-                        <ul class="vertical-nav-menu">
-                            <li class="app-sidebar__heading">Costos CxC/CxP</li>
-                            <li>
-                                <a href="Tprovedores.php">
-                                    <i class="metismenu-icon pe-7s-id"></i>
-                                    Proveedores
-                                </a>
-                            </li>
-                            <li>
-                                <a href="Treglamento.php">
-                                    <i class="metismenu-icon pe-7s-notebook"></i>
-                                    Reglamentos
-                                </a>
-                            </li>
-                            <li>
-                                <a href="tabla-categoria.php">
-                                    <i class="metismenu-icon pe-7s-albums"></i>
-                                    Categorias
-                                </a>
-                            </li>
-                            <li>
-                                <a href="Tproductos.php">
-                                    <i class="metismenu-icon pe-7s-albums"></i>
-                                    Productos
-                                </a>
-                            </li>
-                            <li>
-                                <a href="Vi_producto.php">
-                                    <i class="metismenu-icon pe-7s-albums"></i>
-                                    catalogo
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+        <?php include(ROOT_PATH . "/includes/sidenav.php"); ?>
             <div class="app-main__outer">
                 <div class="app-main__inner">
                     <div class="app-page-title">
@@ -182,23 +87,28 @@ $cat = "SELECT * FROM categoria";
                                                 </tr>
                                             </thead>
                                             <tbody>
-                                                <tr>
-                                                    <?php $resultado = mysqli_query($conectar, $cat);
-
-                                                    while ($row = mysqli_fetch_assoc($resultado)) { ?>
-
-                                                        
-                                                        <td><?php echo $row["nombre"] ?></td>
-
+                                                    <?php
+                                                    $cats_prod = $db->select('categorias_prod', '*');
+                                                    foreach($cats_prod as $catp){
+                                                        ?>
+                                                    <tr>
+                                                        <td><?php echo utf8_encode($catp["nombre_cat"]); ?></td>
                                                         <td>
-                                                            <a href="tabla-editar.php?id=<?php echo $row["id_cat_provedor"]; ?>" type="button" aria-haspopup="true" aria-expanded="false" class="btn-shadow btn btn-info">Editar
+                                                            <a href="tabla-editar.php?id=<?php echo $catp["id_cat_provedor"]; ?>" type="button" aria-haspopup="true" aria-expanded="false" class="btn-shadow btn btn-info">Editar
                                                                 </a>
-                                                            <a href="funciones/eliminar.php?id=<?php echo $row["id_cat_provedor"]; ?>" type="button" aria-haspopup="true" aria-expanded="false" class="btn-shadow btn btn-danger" onclick="return confirmar()">Eliminar
+                                                            <a href="funciones/eliminar.php?id=<?php echo $catp["id_cat_provedor"]; ?>" type="button" aria-haspopup="true" aria-expanded="false" class="btn-shadow btn btn-danger" onclick="return confirmar()">Eliminar
                                                                 </a>
                                                         </td>
-                                                </tr>
-                                            <?php }
-                                                    mysqli_free_result($resultado); ?>
+                                                    </tr>
+                                                        <?php
+                                                    }
+                                                    // $resultado = mysqli_query($conectar, $cat);
+                                                    // while ($row = mysqli_fetch_assoc($resultado)) {
+                                                    ?>
+                                            <?php
+                                            // }
+                                            //     mysqli_free_result($resultado);
+                                            ?>
                                             </tbody>
                                         </table>
                                     </div>
@@ -206,10 +116,17 @@ $cat = "SELECT * FROM categoria";
                             </div>
                         </div>
                     </div>
-                </div>
+                </div>           
+                <?php include(ROOT_PATH . "/includes/footer.php"); ?>
             </div>
         </div>
     </div>
-    <script type="text/javascript" src="./assets/scripts/main.js"></script>
+    <script type="text/javascript" src="../../assets/scripts/main.js"></script>
 </body>
 </html>
+<?php
+    }
+} else {
+    header('Location:' . URL . '/erp_modulos/login/index.php');
+}
+?>
